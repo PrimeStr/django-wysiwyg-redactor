b50fd17c108b16363c52cb1c83a8508f7ac7fb1e
Golovin Maxim <primestr@proninteam.ru>
2024-08-01 13:47:00 +0500

    Fix broken imports for Django 4.2

--- redactor/urls.py
+++ redactor/urls.py
@@ -1,16 +1,14 @@
+from django.urls import path
 
-from django.urls import url
-
-from redactor.views import RedactorUploadView
 from redactor.forms import FileForm, ImageForm
-
+from redactor.views import RedactorUploadView
 
 urlpatterns = [
-    url(r'^upload/image/(?P<upload_to>.*)',
+    path(r'^upload/image/(?P<upload_to>.*)',
         RedactorUploadView.as_view(form_class=ImageForm),
         name='redactor_upload_image'),
 
-    url(r'^upload/file/(?P<upload_to>.*)',
+    path(r'^upload/file/(?P<upload_to>.*)',
         RedactorUploadView.as_view(form_class=FileForm),
         name='redactor_upload_file'),
 ]
