65b89e0bffc718113b36209b4c6285927bf49cc0
Golovin Maxim <primestr@proninteam.ru>
2024-08-01 13:04:33 +0500

    Fix broken imports for Django 4.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ f.close()
 
 setup(
     name='django-wysiwyg-redactor',
-    version='0.5.1.3',
+    version='0.5.1.4',
     description='django-wysiwyg-redactor is a lightweight responsive wysiwyg editor for Django',
     long_description=readme,
     author="Douglas Miranda",
